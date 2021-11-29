d78d4027aa71638c3d20f5bb86a7013e93d83aa5
maxischiavello <maxischiavello12@gmail.com>
2021-11-29 18:43:41 -0300

    primer commit react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+/* function HelloWorld(props) {
+  return (
+    <div id="hello">
+      <h3>{props.subtitle}</h3>
+      {props.mytext}
+    </div>
+  )
+} */
+
+class HelloWorld extends React.Component {
+
+  state = {
+    show: true
+  }
+
+  toggleShow = () => {
+    this.setState({ show: !this.state.show })
+  }
+
+  render() {
+    if (this.state.show) {
+      return (
+        <div id="hello">
+          <h3>{this.props.subtitle}</h3>
+          {this.props.mytext}
+          <button onClick={this.toggleShow}>Toggle show</button>
+        </div>
+      )
+    } else {
+      return <h1>
+        No hay componentes (else)
+        <button onClick={this.toggleShow}>
+          Toggle show
+        </button>
+      </h1>
+    }
+  }
+}
+
+// componente escrito como funcion flecha
+
+// const App = () => <div>This is my component: <HelloWorld /> </div>
+
+
+//componente escrito como clase
+
+/*class App extends React.Component {
+  render() {
+    return <div>This is my component: <HelloWorld /> </div>
+  }
+}*/
+
+//componente escrito como funcion normal
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      This is my component:
+      <HelloWorld mytext="Prueba de ejemplo" subtitle="ejemplo de subtitulo" />
+      <HelloWorld mytext="Segundo ejemplo" subtitle="segundo ejemplo de subtitulo" />
+      <HelloWorld mytext="Tercer ejemplo" subtitle="tercer ejemplo de subtitulo" />
     </div>
   );
 }
