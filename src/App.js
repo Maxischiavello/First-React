4c981ea061bbc5d47c5218239c968a544a7fc887
maxischiavello <maxischiavello12@gmail.com>
2021-12-01 16:07:11 -0300

    learning react second commit

--- src/App.js
+++ src/App.js
@@ -1,69 +1,21 @@
 import React from 'react';
 import './App.css';
+import tasks from './sample/task.json';
+import Tasks from './components/Tasks';
 
-/* function HelloWorld(props) {
-  return (
-    <div id="hello">
-      <h3>{props.subtitle}</h3>
-      {props.mytext}
-    </div>
-  )
-} */
-
-class HelloWorld extends React.Component {
+class App extends React.Component {
 
   state = {
-    show: true
-  }
-
-  toggleShow = () => {
-    this.setState({ show: !this.state.show })
+    tasks: tasks
   }
 
   render() {
-    if (this.state.show) {
-      return (
-        <div id="hello">
-          <h3>{this.props.subtitle}</h3>
-          {this.props.mytext}
-          <button onClick={this.toggleShow}>Toggle show</button>
-        </div>
-      )
-    } else {
-      return <h1>
-        No hay componentes (else)
-        <button onClick={this.toggleShow}>
-          Toggle show
-        </button>
-      </h1>
-    }
-  }
-}
-
-// componente escrito como funcion flecha
-
-// const App = () => <div>This is my component: <HelloWorld /> </div>
-
-
-//componente escrito como clase
-
-/*class App extends React.Component {
-  render() {
-    return <div>This is my component: <HelloWorld /> </div>
-  }
-}*/
-
-//componente escrito como funcion normal
-
-function App() {
-  return (
-    <div>
-      This is my component:
-      <HelloWorld mytext="Prueba de ejemplo" subtitle="ejemplo de subtitulo" />
-      <HelloWorld mytext="Segundo ejemplo" subtitle="segundo ejemplo de subtitulo" />
-      <HelloWorld mytext="Tercer ejemplo" subtitle="tercer ejemplo de subtitulo" />
+    return <div>
+      <Tasks tasks={this.state.tasks} />
     </div>
-  );
+  }
 }
+
+
 
 export default App;
---
+++ src/components/Task.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Task extends React.Component {
+    render() {
+        const {task} = this.props;
+
+        return <div>
+            {task.title} -
+            {task.description} -
+            {task.done} -
+            {task.id}
+            < input type="checkbox" />
+            <button>
+                X
+            </button>
+        </div>
+    }
+}
+
+export default Task;
