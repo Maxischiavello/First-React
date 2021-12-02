6598a2fabf7c3a26f7c270992294b50ee6682d65
maxischiavello <maxischiavello12@gmail.com>
2021-12-02 14:43:29 -0300

    estilos como objetos js en react

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,20 +1,40 @@
 import React from 'react';
 
 class Task extends React.Component {
+
+    styleCompleted() {
+        return {
+            fontSize: '20px',
+            color: this.props.task.done ? 'green' : 'red',
+            textDecoration: this.props.task.done ? 'line-through' : 'none'
+        }
+    }
+
     render() {
         const {task} = this.props;
 
-        return <div>
+        return <div style={this.styleCompleted()}>
             {task.title} -
             {task.description} -
             {task.done} -
             {task.id}
             < input type="checkbox" />
-            <button>
+            <button style={btnDelete}>
                 X
             </button>
         </div>
     }
+}
+
+const btnDelete = {
+    fontSize: '18px',
+    background: '#ea2027',
+    color: '#fff',
+    border: 'none',
+    padding: '10px 15px',
+    margin: '10px',
+    borderRadius: '50%',
+    cursor: 'pointer'
 }
 
 export default Task;
